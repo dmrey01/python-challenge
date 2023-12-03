2b2e48f5280e22c8a4b61b5422863fe3b403024f
First user <140979441+dmrey01@users.noreply.github.com>
2023-12-03 11:33:56 -0600

    main.py

---
+++ PyBank/analysis/main.py
@@ -0,0 +1,45 @@
+import os
+import csv
+
+#csvpath = os.path.join('..', 'Resources','budget_data.csv')
+#file =  "../resource/analysis.txt"
+
+with open(file, "r") as file:
+	reader = csv.reader(file)
+	header - next(reader)
+	for row in reader: 
+
+return Column_data
+
+file_path = "budget_data.csv"
+Column_index =1
+profit_losses-data = get_column_data(file_path,Column_index)
+print("profit/Lossesdata:". profit_losses_data)
+
+
+#lines = text.read()
+
+#print(lines)
+
+#List to store data
+Column_data =[]
+num_mths =[]
+
+
+
+for row in csvreader:
+	Column_date.append(row[0])
+
+	profit/loss.append(row[1])
+
+
+
+
+
+
+
+
+
+ #output_file = os.path.join("budget_data.csv")
+
+ #print(num_mths)
